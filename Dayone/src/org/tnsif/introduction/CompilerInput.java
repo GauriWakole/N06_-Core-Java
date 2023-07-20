6575e1981cdd40d2faa27413259c1bd34eae47ce
ADMIN <ADMIN@DESKTOP-N0RNQ4J>
2023-07-20 14:07:16 +0530

    first commit

---
+++ Dayone/src/org/tnsif/introduction/CompilerInput.java
@@ -0,0 +1,14 @@
+//Pogram to demonstrate pn compile-Time-Input
+package org.tnsif.introduction;
+
+public class CompilerInput {
+
+	public static void main(String[] args) 
+	{
+		//variable initialization
+		short num=34;
+		System.out.println("The num is: "+num);
+
+	}
+
+}
---
+++ Dayone/src/org/tnsif/introduction/UserInput.java
@@ -0,0 +1,31 @@
+//program to demonstrate on user-input value
+package org.tnsif.introduction;
+import java.util.Scanner;
+public class UserInput {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string: ");
+		String str1=sc.next();
+		System.out.println("string is: "+str1);
+	
+	
+		System.out.println("Enter the string: ");
+		String str2=sc.nextLine();
+		System.out.println("string is: "+str2);
+	
+	
+	
+		System.out.println("Enter the Integer: ");
+		int num=sc.nextInt();
+		System.out.println("string is: "+num);
+	
+
+	
+		System.out.println("Enter the character: ");
+		char c=sc.next().charAt(0);
+		System.out.println("string is: "+c);
+	
+	}
+}
